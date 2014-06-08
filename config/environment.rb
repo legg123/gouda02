f76fc47b9a5b02e78cf219cc32978f88a2921faa
Lucio E. Gonzalez <lucioeduardogonzalezgonzalez@gmail.com>
2014-06-08 13:52:38 -0500

    Inicializa Gouda02

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Gouda02::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gouda02::Application.config.secret_key_base = '0d9a3d34d71d948e472e56b3d7785a5aff0c90bd04065ff952de072d9213957665ffe8854767e5b9dc32d05bb4f87e02c35683f1edbda2fa146070d3afc2886f'
